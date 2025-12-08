55ea5e967b598d9ffef8eb749cc12c401d97777d
gkbishnoi07 <gkishan1kyt@gmail.com>
2025-12-08 23:29:40 +0530

    remove replit

--- vite.config.ts
+++ vite.config.ts
@@ -11,14 +11,7 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
-    port: 5000,
-    strictPort: true,
-    allowedHosts: true,
-    hmr: {
-      clientPort: 5000,
-    },
+    port: 3000,
+    open: true,
   },
 });
-
-
